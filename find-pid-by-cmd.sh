f429b426966dbe2a910eb98ecd2c02f9a5998c28
Evan Johnson <ev.jonnson@gmail.com>
2024-04-18 01:25:01 -0600

    find pid via argument

--- find-pid-by-cmd.sh
+++ find-pid-by-cmd.sh
@@ -1,6 +1,6 @@
 # kill process by specifying the exact string to match the command
-targetProcess="/Applications/MidiPipe.app/Contents/MacOS/MidiPipe"
-processList=$(ps -axo pid,comm | grep "/Applications/MidiPipe.app/Contents/MacOS/MidiPipe")
+targetProcess=$1
+processList=$(ps -axo pid,comm | grep $targetProcess)
 while read -r line; do
     pid=$(echo "$line" | awk '{print $1}')
     comm=$(echo "$line" | awk '{$1=""; print $0}' | sed 's/^[[:space:]]*//')
