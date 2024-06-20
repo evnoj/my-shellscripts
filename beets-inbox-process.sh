b2846c20bbecbceb54d1fe210b3c04326adf9d7d
Evan Johnson <ev.jonnson@gmail.com>
2024-06-20 15:34:31 -0600

    unzip to dir and trash the zip

--- beets-inbox-process.sh
+++ beets-inbox-process.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/bin/zsh
 set -euo pipefail
 
 inboxdir=$HOME/tunes/inbox
 for zip in $inboxdir/zipped/*.zip; do
-    # unzip "$zip" -d $inboxdir/unzipped
     name=${zip%.zip}
     name=$(basename "$name")
-    mkdir "$inboxdir/unzipped/$name"
+    dirpath="$inboxdir/unzipped/$name"
+
+    mkdir "$dirpath"
+    unzip "$zip" -d "$dirpath"
+    trash "$zip"
 done
 
     
