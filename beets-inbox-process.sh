0b1f7c7de61555459d911711c0c8dccde2d7809f
Evan Johnson <ev.jonnson@gmail.com>
2024-08-01 21:20:25 -0600

    beets import fixing

--- beets-inbox-process.sh
+++ beets-inbox-process.sh
@@ -10,20 +10,29 @@ for zip in "$inboxdir"/zipped/*.zip; do
     dirpath="$inboxdir/unzipped/$name"
 
     mkdir "$dirpath"
-    unzip "$zip" -d "$dirpath"
+    # unzip "$zip" -d "$dirpath"
+    /usr/local/opt/unzip/bin/unzip -I $(chardetect --minimal <<<"$zip") "$zip" -d "$dirpath"
     trash "$zip"
 done
 
 # import albums, moving (not copying) successfully imported files
-beet import -imq -l "$inboxdir/beet-import.log" "$inboxdir/unzipped/"*
+# use beets installed in the user-utilities venv
+~/.venv/bin/python -m beets import -imq -l "$inboxdir/beet-import.log" "$inboxdir/unzipped/"*
 
 # go through directories and remove any that only contain a cover image (a.k.a. ones that were successfully imported)
 for dir in "$inboxdir/unzipped/"*; do
-    dircontents="$(ls -A1 "$dir" | grep -vxF .DS_Store)" # filter .DS_Store
-    if [[ "$dircontents" = "cover."* ]] && # file found is a cover
-    [[ "$(echo "$dircontents" | wc -l)" -eq 1 ]] # only one file found
+    set -- "$dir"/*
+    # if only 1 file and its a cover
+    if [[ "$#" == 1 ]] && [[ "$1" == "cover.*" ]] 
     then
         trash "$dir"
     fi
+    
+    # dircontents="$(ls -A1 "$dir" | grep -vxF .DS_Store)" # filter .DS_Store
+    # if [[ "$dircontents" = "cover."* ]] && # file found is a cover
+    # [[ "$(echo "$dircontents" | wc -l)" -eq 1 ]] # only one file found
+    # then
+    #     trash "$dir"
+    # fi
 done
 
