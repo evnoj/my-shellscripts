03db1e78235afd34ae0be9461710c5f4461fa939
Evan Johnson <ev.jonnson@gmail.com>
2025-01-03 12:13:15 -0700

    document the script

---
+++ open-webpage-iframe.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# opens a webpage in an iframe to get around the Cold Turkey Blocker
+# pass the webpage's URL as the sole argument to the script
+# intended to be able to access reddit/HN pages that appear in search results
+#
+# requirements:
+# macOS
+# Python 3
+# wget
+
+
