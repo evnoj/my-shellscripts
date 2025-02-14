5459453b1686008a828febc8dee197f7126d9590
Evan Johnson <ev.jonnson@gmail.com>
2025-02-14 12:48:29 -0700

    update refresh scripts

--- tomatofre.sh
+++ tomatofre.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
+# reloads the launchagent that sets the wallpaper to red when tomatobar has a work timer going
+# run this script while tomatobar is not in work state (can be running)
 launchctl unload ~/Library/LaunchAgents/local.tomatobar-ev-watcher.plist && \
 launchctl load -w ~/Library/LaunchAgents/local.tomatobar-ev-watcher.plist
