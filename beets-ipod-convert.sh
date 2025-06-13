053caa824738e42f1812765a3746657e1471fa9d
Evan Johnson <ev.jonnson@gmail.com>
2025-06-12 22:49:52 -0600

    take arg instead of hardcoded path

--- beets-ipod-convert.sh
+++ beets-ipod-convert.sh
@@ -1,7 +1,9 @@
 #!/usr/local/bin/bash
-# converts the albums found in ~/tunes/ipod-albums-list.txt into ipod-appropriate format and puts them in the Music.app folder that will automatically import them the next time Music.app is open
+# converts the albums found in the argument (ex. ~/tunes/ipod-albums-list.txt) into ipod-appropriate format and puts them in the Music.app folder that will automatically import them the next time Music.app is open
+# to generate ipod-albums-list in the correct format, run:
+# beets ls -af '$albumartist :: $album' > ~/tunes/albums-list.txt
 
-cat ~/tunes/ipod-albums-list.txt |
+cat "$1" |
 kak -f 's::<ret>lGhd' |
 tr '\n' '\0' |
-xargs -0 -n1 -P0 -I {} beet convert -d '~/Music/Apple Music/Automatically Add to Music.localized' -f ipod -a -y "album:={}"
+xargs -0 -n1 -P0 -I {} beets convert -d '~/tunes/ipod-exports' -f ipod -a -y "album:={}"
