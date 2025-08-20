521009d4615c647c51d95854ce4effe55661d281
Evan Johnson <ev.jonnson@gmail.com>
2025-08-20 14:23:00 -0600

    heic conversion script takes path arg

--- convert-heic-to-jpg.sh
+++ convert-heic-to-jpg.sh
@@ -1,14 +1,16 @@
 #!/bin/zsh
-# converts all files in the pwd with a .heic or .HEIC extension to a jpg, with the same pre-extension filename
+# converts all files in the passed directory (or pwd if not provided) with a .heic or .HEIC extension to a jpg, with the same pre-extension filename
 # jpgs are output in the pwd
 # requirements: macOS
 
 setopt nullglob
 
-for i in *.heic(:r); do
+1="${1:-.}"
+
+for i in "$1"/*.heic(:r); do
     sips -s format jpeg -s formatOptions best "$i.heic" --out "$i.jpg"
 done
 
-for i in *.HEIC(:r); do
+for i in "$1"/*.HEIC(:r); do
     sips -s format jpeg -s formatOptions best "$i.HEIC" --out "$i.jpg"
 done
